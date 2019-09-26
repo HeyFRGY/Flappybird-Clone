7ec88a273556e6a5f5b48f299099fc1c3e776fd2
Micheal Fergerson <mikefergerson@gmail.com>
2019-09-26 15:45:16 +0900

    Initial Commit
    I just wanted to make a clone of a very popular iOS game. I used a number of different guides and looked at others programming to get this done. I wanted to make it a battle royale but that didn't work in my favor.

---
+++ Flappybird/FlappyBirdTests/FlappyBirdTests.swift
@@ -0,0 +1,35 @@
+//
+//  AppDelegate.swift
+//  FlappyBird Clone
+//
+//  Created by AV Japan on 2018/01/29.
+//  Copyright © 2019 FRGY. All rights reserved.
+//
+
+import XCTest
+
+class FlappyBirdTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
